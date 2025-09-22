6f6fcf41289406da8c12a314dd2491f66ee0422b
cpf <cpfprogrammer@163.com>
2025-09-22 20:03:56 +0800

    redis复习初级阶段完成.

--- src/test/java/com/cpf/learn/helloredis/test/TestOriginRedisTemplate.java
+++ src/test/java/com/cpf/learn/helloredis/test/TestOriginRedisTemplate.java
@@ -5,9 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-
 /**
  * @author cui pengfei
  * @since 17
--- src/test/java/com/cpf/learn/helloredis/test/TestStringRedisTemplate.java
+++ src/test/java/com/cpf/learn/helloredis/test/TestStringRedisTemplate.java
@@ -3,11 +3,7 @@ package com.cpf.learn.helloredis.test;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 /**
  * @author cui pengfei
